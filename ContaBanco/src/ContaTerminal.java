13ed606e368a6710dba5515871f79da7f774197e
Paulo Tanini <paulo.tanini07gmail.com>
2024-05-24 14:48:00 -0300

    projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
+    
     public static void main (String args[]) throws Exception{
-        System.out.println("Olá");    
-    }
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Por favor, informe o número da agência: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, informe a sua agência: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, informe o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Por favor indique o seu saldo bancário: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá" + nome + " ,obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" ,conta "+ numero +" e o seu saldo é de: " + saldo +" já está disponível para saque.");
+        }
 }
